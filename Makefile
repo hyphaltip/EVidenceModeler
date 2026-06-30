b96dd6e877e18ce06ac2615f68f1964000057165
Jason Stajich <jasonstajich.phd@gmail.com>
2026-06-30 08:28:08 -0700

    phase F: packaging, CI, docs
    - Bump all crate versions to 2.1.0 (matching Perl release line)
      using workspace.package version inheritance
    - Update root Makefile: default target builds Rust release binaries
      via cargo; legacy parafly target preserved
    - Update testing/Makefile: default test target now runs runMe.rust.sh
    - Update testing/runMe.sh to invoke the Rust EVidenceModeler binary
      (was Perl ../EVidenceModeler)
    - Rewrite README.md with Rust build/install/usage instructions
    - Add Changelog.txt entry for the Rust rewrite (2.1.0)

--- Makefile
+++ Makefile
@@ -1,15 +1,24 @@
-all: parafly
+all: rust
 
+# --- Rust build (default) ---
+rust:
+	cargo build --release --manifest-path evm/Cargo.toml
+
+rust-debug:
+	cargo build --manifest-path evm/Cargo.toml
+
+test:
+	cargo test --manifest-path evm/Cargo.toml --all
+	cd testing && bash runMe.rust.sh
+
+# --- Legacy ParaFly build (only needed for Perl EVM) ---
 OS := $(shell uname)
 
 CXX = g++
 CC = gcc
 
 parafly:
-	cd plugins/ParaFly && sh ./configure --prefix=`pwd` CXX=$(CXX) CC=$(CC) CFLAGS="-fopenmp" CXXFLAGS="-fopenmp" && $(MAKE) install 
-
-
+	cd plugins/ParaFly && sh ./configure --prefix=`pwd` CXX=$(CXX) CC=$(CC) CFLAGS="-fopenmp" CXXFLAGS="-fopenmp" && $(MAKE) install
 
 large_sample_data:
 	git clone https://github.com/EVidenceModeler/EVM_sample_data.git
-
--- testing/runMe.sh
+++ testing/runMe.sh
@@ -1,18 +1,26 @@
 #!/bin/sh
-
 set -ex
 
-## run EVM
+## run Rust EVM pipeline test
+cd "$(dirname "$0")"
 
-../EVidenceModeler --sample_id smalltest \
-                   --genome genome.fasta \
-                   --weights ./weights.txt \
-                   --gene_predictions gene_predictions.gff3 \
-                   --protein_alignments protein_alignments.gff3 \
-                   --transcript_alignments transcript_alignments.gff3 \
-                   --segmentSize 100000 \
-                   --overlapSize 10000
+RUST_BIN="../evm/target/debug/EVidenceModeler"
+if [ ! -x "$RUST_BIN" ]; then
+    RUST_BIN="../evm/target/release/EVidenceModeler"
+fi
+if [ ! -x "$RUST_BIN" ]; then
+    echo "Rust binary not found. Run: cargo build --manifest-path ../evm/Cargo.toml"
+    exit 1
+fi
 
-      
-echo "Done. See smalltest.EVM.* outputs"
+"$RUST_BIN" \
+    --sample_id rusttest \
+    --genome genome.fasta \
+    --weights ./weights.txt \
+    --gene_predictions gene_predictions.gff3 \
+    --protein_alignments protein_alignments.gff3 \
+    --transcript_alignments transcript_alignments.gff3 \
+    --segmentSize 100000 \
+    --overlapSize 10000
 
+echo "Done. See rusttest.EVM.* outputs"
